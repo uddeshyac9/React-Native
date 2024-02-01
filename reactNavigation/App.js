9f9eef9c60e879b13d1e125930c4c4fd754f3fb6
uddeshyac9 <uddeshyac9@gmail.com>
2024-02-01 22:39:59 +0530

    added-login.jsx

--- reactNavigation/App.js
+++ reactNavigation/App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './components/Home';
 import About from './components/About';
+import Login from './components/Login';
 
 
 function App() {
@@ -11,6 +12,7 @@ const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login}/>
         <Stack.Screen name='Home' component={Home}/>
         <Stack.Screen name='About' component={About}/>
       </Stack.Navigator>
---
+++ reactNavigation/components/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const Login = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your authentication logic here
+    // For simplicity, let's assume login is successful for any input
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        placeholderTextColor="black"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="black"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <Text style={styles.link} onPress={() => navigation.navigate('About')}>
+        Go to About without login
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+    color:'black'
+
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    color:'blue'
+  },
+  link: {
+    marginTop: 20,
+    color: 'blue',
+  },
+});
+
+export default Login;
